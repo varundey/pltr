bbaa0c1b63865f07b28bcdbe19c1a2eac48b9d3c
Varun Dey <varun_dey@symantec.com>
2019-08-02 15:40:53 +0530

    Inject div as first child of body

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-alert("hello");
+const bodyNode = document.body;
+const progressNode = document.createElement('div');
+progressNode.class = 'pltr';
+bodyNode.insertBefore(progressNode, bodyNode.firstChild);
