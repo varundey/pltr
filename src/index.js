c09e3045eb20d175d59c49036dec4e3520f7c933
Varun Dey <varundey20@gmail.com>
2019-08-03 15:36:25 +0530

    Load progress on scroll

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const bodyNode = document.body;
 const progressNode = document.createElement('div');
 progressNode.setAttribute('class', 'pltr');
 bodyNode.insertBefore(progressNode, bodyNode.firstChild);
+
+/**
+ * Formula at the bottom of the page
+ * scrollHeight + clientHeight = documentHeight
+ * We need to find out percentage of SH + CH w.r.t DH
+ * @type {number}
+ */
+window.addEventListener('scroll', () => {
+  const progress = 100 - ((document.body.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight))/document.body.scrollHeight)*100;
+  document.querySelector('.pltr').style.width = `${progress}%`;
+});
