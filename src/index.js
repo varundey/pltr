40b3803e107d3bbaefe59d5893845cb22a246b9d
Varun Dey <varun_dey@symantec.com>
2019-08-03 16:06:54 +0530

    Update index.js to have separate functions + show progress on page load

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ const bodyNode = document.body;
 const progressNode = document.createElement('div');
 progressNode.setAttribute('class', 'pltr');
 bodyNode.insertBefore(progressNode, bodyNode.firstChild);
+const pltrNode = document.querySelector('.pltr');
 
 /**
  * Formula at the bottom of the page
  * scrollHeight + clientHeight = documentHeight
  * We need to find out percentage of SH + CH w.r.t DH
- * @type {number}
  */
+const calculateProgress = () => {
+  const documentHeight = document.body.scrollHeight;
+  const scrollHeight = window.pageYOffset;
+  const clientHeight = document.documentElement.clientHeight;
+  return 100 - ((documentHeight - (scrollHeight + clientHeight)) / documentHeight) * 100;
+};
+
+const updateProgress = (progress) => {
+  pltrNode.style.width = `${progress}%`;
+};
+
+updateProgress(calculateProgress());
+
 window.addEventListener('scroll', () => {
-  const progress = 100 - ((document.body.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight))/document.body.scrollHeight)*100;
-  document.querySelector('.pltr').style.width = `${progress}%`;
+  const progress = calculateProgress();
+  updateProgress(progress);
 });
