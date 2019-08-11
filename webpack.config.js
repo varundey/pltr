c1d30ab3cb8b58b5f86d68d85c3f064523da3e98
Varun Dey <varundey20@gmail.com>
2019-08-11 23:31:28 +0530

    Add Copy plugin to copy manifest files to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/pltr.js',
     output: {
         filename: 'pltr.js',
         path: path.resolve(__dirname, 'dist/src')
-    }
+    },
+    plugins: [
+        new CopyPlugin([{
+            from: 'manifest.json',
+            to: path.resolve(__dirname, 'dist')
+        }])
+    ]
 };
