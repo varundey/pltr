6166d5f6448172704e618c3b25abc783f1c1a5bf
Varun Dey <varundey20@gmail.com>
2019-08-12 00:08:18 +0530

    Add Clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/pltr.js',
@@ -11,6 +12,9 @@ module.exports = {
         new CopyPlugin([{
             from: 'manifest.json',
             to: path.resolve(__dirname, 'dist')
-        }])
+        }]),
+        new CleanWebpackPlugin({
+            verbose: true
+        })
     ]
 };
